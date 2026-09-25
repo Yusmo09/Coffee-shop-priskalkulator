076d63e76aa7adc935ef6c345b83eaff3c4865a0
Yusmo09 <theoaase09@gmail.com>
2026-09-25 10:17:56 +0200

    Holder på å lage noe kode om løkker og enklere kod

--- coffeeshop.py
+++ coffeeshop.py
@@ -16,15 +16,56 @@ print(" > Mocha")
 print(" > Flat White")
 print("----------------------------")
 
+#Når koden starter blir prisen resettet til 0 og så spør programmet hva kaffe personen ønsker
 price = 0
-coffee = input("What type of coffee would you like?").title()
+size = 0
+coffee = input("What type of coffee would you like? ").title()
+
+#Her velger eg prisen for alle kaffe typene
 if coffee=="Espresso":
    price = price + 2.50
+
 elif coffee=="Americano":
    price = price + 3
+
 elif coffee=="Latte":
    price = price + 2.50
 
-#Complete the code here...
+elif coffee == "Cappuccino":
+   price = price + 3
+
+elif coffee == "Macchiato":
+   price = price + 2.50
+
+elif coffee == "Mocha":
+   price = price + 3.50
+
+elif coffee == "Flat White":
+   price = price + 2.50
+
+#Her spør den hva størrelse kunden ønsker på drikken sin
 print("----------------------------")
-print("Total Cost: £" + str(price))
+print("We have the following sizes")
+print("Medium")
+print("Large")
+print("XL")
+size = input("What size would you like: ")
+if size == "Medium":
+   size = size + 0
+
+elif size == "Large":
+   size = size + 1
+
+elif size == "XL":
+   size = size + 1.5
+
+print("----------------------------")
+takeAway = input("Would you like take away, yes or no?: ")
+if takeAway == "no":
+   price = price + 0
+
+elif takeAway == "yes":
+   price = price + 1
+print("----------------------------")
+total = price + size
+print(f"Total Cost: £ {price} + {size} = {total}")
